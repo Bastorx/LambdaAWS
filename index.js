3182e4d0190c754c5ec060f441d8d475237527d2
Sinthuvan Vigneswaran <vignes_s@etna-alternance.net>
2016-04-17 12:03:10 +0200

    add context

--- index.js
+++ index.js
@@ -90,7 +90,7 @@ const convert = (event, callback) => {
 	});
 }
 
-exports.handler = (event, callback) => {
+exports.handler = (event, context, callback) => {
     aws.config.loadFromPath('./package.json');
 
     const op = event.op;
