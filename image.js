887b1644bbf294f01ecea420ce8ef6269ccf739c
Trololol94 <adrienalexandre5@gmail.com>
2016-04-17 15:14:26 +0200

    save last good version$

--- image.js
+++ image.js
@@ -1,124 +1,127 @@
 var fs = require('fs');
 var gm = require('gm');
+var aws = require('aws-sdk');
 
-/////// BLUR ///////
-var event = {op:'blur', link:'http://www.fastandfood.fr/wp-content/uploads/2016/03/emoji_gaufre-300x202@2x.jpg', name:'cropped-North.jpg', p1:30, p2:10};
-/////// CROP ///////
-// var event = {op:'crop', name:'cat.png', p1:250, p2:250, p3:10, p4:10};
-/////// RESIZE ///////
-// var event = {op:'resize', name:'max-small_height.jpg', p1:700, p2:500};
-/////// ROTATE ///////
-// var event = {op:'rotate', name:'cat_fun.jpg', p1:220};
-/////// SEPIA ///////
-// var event = {op:'sepia', name:'cropped-North.jpg'};
-/////// CONVERT ///////
-// var event = {op:'convert', name:'cropped-North.jpg', p1:'png'};
-
-
-// var event = {op:'crop', name:'cat.png', p1:250, p2:250, p3:10, p4:10};
 var error = "Operation requested";
 
-const blur = (event) => {
+const blur = (event, callback) => {
     gm(event.link)
-	.blur(event.p1, event.p2)
-	.write(event.name, function(err){
+	.blur(event.blur1, event.blur2)
+	.write(event.name, function(err, stdout){
 	    if (err) throw err
-		console.log("Written montage image.")
-	});
-	fs.readFile(event.name, function(err, data) {
-		if (err) throw err;	
-		console.log("etape de l'encode");	
+	    fs.readFile(event.name, function(err, data) {
 		var encodeImage = new Buffer(data, 'binary').toString('base64');
-		console.log(encodeImage)
-		return encodeImage;
+		callback(null, encodeImage);
+	    });
 	});
 };
 
-const crop = (event) => {
-    gm(event.name)
-	.crop(event.p1, event.p2, event.p3, event.p4)
+const crop = (event, callback) => {
+    gm(event.link)
+	.crop(event.crop1, event.crop2, event.crop3, event.crop4)
 	.write(event.name, function(err){
 	    if (err) throw err
-		console.log("Written montage image.")
+	    fs.readFile(event.name, function(err, data) {
+		var encodeImage = new Buffer(data, 'binary').toString('base64');
+		callback(null, encodeImage);
+	    });
 	});
 };
 
-const resize = (event) => {
-	if (event.p3=='!') {
-    	gm(event.name)
-		.resize(event.p1, event.p2, event.p3)
-		.write(event.name, function(err){
-		    if (err) throw err
-	    	console.log("Written montage image.");
+const resize = (event, callback) => {
+    if (event.resize3=='!') {
+    	gm(event.link)
+	    .resize(event.resize1, event.resize2, event.resize3)
+	    .write(event.name, function(err){
+		if (err) throw err
+		fs.readFile(event.name, function(err, data) {
+		    var encodeImage = new Buffer(data, 'binary').toString('base64');
+		    callback(null, encodeImage);
 		});
-		return event;
-	}
-	else {
-		gm(event.name)
-		.resize(event.p1, event.p2)
-		.write(event.name, function(err){
-	   	 if (err) throw err
-	   		console.log("Written montage image.");
+	    });
+    }
+    else {
+	gm(event.link)
+	    .resize(event.resize1, event.resize2)
+	    .write(event.name, function(err){
+	   	if (err) throw err
+		fs.readFile(event.name, function(err, data) {
+		    var encodeImage = new Buffer(data, 'binary').toString('base64');
+		    callback(null, encodeImage);
 		});
-		return event;
-	}
+	    });
+    }
 };
 
-const rotate = (event) => {
-    gm(event.name)
-	.rotate("white", event.p1)
+const rotate = (event, callback) => {
+    gm(event.link)
+	.rotate("white", event.rotate1)
 	.write(event.name, function(err){
 	    if (err) throw err
-	    console.log("Written montage image.")
+	    fs.readFile(event.name, function(err, data) {
+		var encodeImage = new Buffer(data, 'binary').toString('base64');
+		callback(null, encodeImage);
+	    });
 	});
 };
 
-const sepia = (event) => {
-	gm(event.name)
+const sepia = (event, callback) => {
+    gm(event.link)
 	.sepia()
 	.write(event.name, function (err) {
 	    if (err) throw err
-	    console.log("Written montage image.")
+	    fs.readFile(event.name, function(err, data) {
+		var encodeImage = new Buffer(data, 'binary').toString('base64');
+		callback(null, encodeImage);
+	    });
 	});
 };
 
-const convert = (event) => {
-	var coolVar = event.name;
-	var partsArray = coolVar.split('.');
-	gm(event.name)
-		.write(partsArray[0]+"."+event.p1, function (err) {
+const convert = (event, callback) => {
+    var coolVar = event.name;
+    var partsArray = coolVar.split('.');
+    gm(event.name)
+	.write(partsArray[0]+"."+event.convert1, function (err) {
 	    if (err) throw err
-	    console.log("Written montage image.")
+	    fs.readFile(event.name, function(err, data) {
+		var encodeImage = new Buffer(data, 'binary').toString('base64');
+		callback(null, encodeImage);
+	    });
 	});
 }
 
+exports.handler = (event, context, callback) => {
+    aws.config.loadFromPath('./package.json');
 
-// exports.handler = (event) => {
-const op = event.op
-delete event.op;
-if (!op) {Ô
+    const op = event.op;
+    delete event.op;
+    var i = 0;
+    if (!op) {
 	console.log(error);
-}
-switch (op) {
+    }
+    while(typeof op[i] !== 'undefined') {
+	switch (op[i]) {
 	case 'blur':
-		blur(event);
-		break;
+	    blur(event, callback);
+	    break;
 	case 'resize':
-		resize(event);
-		break;
+	    resize(event, callback);
+	    break;
 	case 'crop':
-		crop(event);
-		break;
+	    crop(event, callback);
+	    break;
 	case 'rotate':
-		rotate(event);
-		break;
+	    rotate(event, callback);
+	    break;
 	case 'sepia':
-		sepia(event);
-		break;
+	    sepia(event, callback);
+	    break;
 	case 'convert':
-		convert(event);
-		break;
+	    convert(event, callback);
+	    break;
 	default :
-		console.log(new Error("Unrecognized operation ${op}"));
+	    callback(new Error("Unrecognized operation ${op}"));
 	}
-// };
+	i++;
+    }
+};
